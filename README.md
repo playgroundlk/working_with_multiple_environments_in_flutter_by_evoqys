5b3beac76d78e232d9c996b7b61d88bea5e5b43d
thilokaviraj <thilokaviraj@gmail.com>
2024-09-30 11:18:08 +0530

    Create Flutter project

--- README.md
+++
@@ -1,2 +0,0 @@
-# working_with_multiple_environments_in_flutter_by_evoqys
-Learn the working of multiple environments in flutter directly from scratch.
---
+++ android/app/src/main/kotlin/com/example/working_with_multiple_environments_in_flutter_by_evoqys/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.working_with_multiple_environments_in_flutter_by_evoqys
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
